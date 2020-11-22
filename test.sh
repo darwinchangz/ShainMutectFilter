38fc7236d483e5fb95b88dbd43439f71d1424378
darwinchangz <45807256+darwinchangz@users.noreply.github.com>
2020-11-22 14:03:57 -0800

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-module load CBC samtools r
-cd ~/test
-~/Mutect2_GitHub/Mutect2_Filter.sh ~/test/GI-10.funcotator.txt ~/wget/bams/GI-10_T.bam ~/wget/bams/GI-10_N.bam ~/hg19/hg19.fa male
